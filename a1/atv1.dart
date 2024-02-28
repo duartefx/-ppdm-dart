8cc82869f5e48aee8b84d02249c8da5ded540427
Giovana Duarte <133895005+duartefx@users.noreply.github.com>
2024-02-28 07:34:14 -0300

    Add files via upload

---
+++ a1/atv1.dart
@@ -0,0 +1,9 @@
+void main(){
+  int a =20;
+
+  if (a % 5==0){
+    print('O número $a '  ' é múltiplo de 5');
+  } else{
+    print("O NÚMERO $a não é múltiplo de 5");
+  }
+}
---
+++ a1/atv2.dart
@@ -0,0 +1,9 @@
+void main(){
+  var num = 20;
+
+  // Operador ternário
+  var mensagem = num % 2 == 0 ? 'O número $num é par. ' : 'O número $num é impar';
+
+  //Imprimir a mensagem 
+  print(mensagem);
+}
