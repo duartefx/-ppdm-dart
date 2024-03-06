59790322653dc7b1ca9a36dc4765ff16f359d727
Giovana Duarte <133895005+duartefx@users.noreply.github.com>
2024-03-06 10:46:37 -0300

    Add files via upload

---
+++ list/exercicio2.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<String> animais = ["coelho", "pardal", "gato", "rato", "sapo"];
+  List<String> frutas = ["mamão", "abacaxi", "maçã", "uva", "morango"];
+
+  List<String> lista = animais + frutas;
+  print(lista);
+}
